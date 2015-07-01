9b147f5b7ce0d60b630a3e794308822a22f7164b
samczsun <samczsun@gmail.com>
2015-07-01 18:53:37 -0400

    Remove other extraneous things

--- src/main/java/com/samczsun/skype4j/serialize/JsonSerializable.java
+++
@@ -1,9 +0,0 @@
-package com.samczsun.skype4j.serialize;
-
-import com.google.gson.JsonObject;
-
-public interface JsonSerializable {
-    public JsonObject serialize();
-
-    public void deserialize(JsonObject object);
-}
