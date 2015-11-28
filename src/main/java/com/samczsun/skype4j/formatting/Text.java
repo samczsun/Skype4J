2250e16b3867fdf6cfa78b664cf22147599ead15
Sam Sun <samczsun@users.noreply.github.com>
2015-11-27 23:11:10 -0500

    Revert "Enhance RichText to support formats per component"

--- src/main/java/com/samczsun/skype4j/formatting/Text.java
+++ src/main/java/com/samczsun/skype4j/formatting/Text.java
@@ -50,28 +50,8 @@ public abstract class Text {
      * Creates a new RichText component
      * @return A new RichText object
      */
-    public static RichText rich(String text) {
-        return new RichText(text);
-    }
-
-    /**
-     * Creates a new RichText component
-     * @return A new RichText object
-     */
-    public static RichText rich(String format, Object...params) {
-        Validate.notNull(format, "Format was null");
-        Validate.notNull(params, "Parameters were null. If you don't want to pass any, consider plain(String)");
-        return rich(String.format(format, params));
-    }
-
-    /**
-     * Creates a new PlainText component with the given text
-     * @return The PlainText object representing the text
-     */
-    public static PlainText plain(String format, Object...params) {
-        Validate.notNull(format, "Format was null");
-        Validate.notNull(params, "Parameters were null. If you don't want to pass any, consider plain(String)");
-        return plain(String.format(format, params));
+    public static RichText rich() {
+        return new RichText();
     }
 
     /**
